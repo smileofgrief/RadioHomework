8b82617a6c36f426b2a6dd554ce64aba08b6cb8d
Marina Kuchmasova <kuchmasova.m@gmail.com>
2023-03-12 15:16:50 +0300

    Add RadioClass

---
+++ src/main/java/Radio.java
@@ -0,0 +1,32 @@
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > 9) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10 ){
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+}
